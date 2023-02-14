8cc7c62aa211eabb4555d3fa8a165da1ee1a239e
Krystyna Kruchkovska <kruchkovskakr@gmail.com>
2023-02-14 09:31:59 +0100

    Add: crossfading animation

--- 06-view-animations-in-practice/projects/starter/Flight Info/ViewController.swift
+++ 06-view-animations-in-practice/projects/starter/Flight Info/ViewController.swift
@@ -40,43 +40,43 @@ func delay(seconds: Double, completion: @escaping () -> Void) {
 
 class ViewController: UIViewController {
   @IBOutlet var bgImageView: UIImageView!
-
+  
   @IBOutlet var summaryIcon: UIImageView!
   @IBOutlet var summary: UILabel!
-
+  
   @IBOutlet var flightNr: UILabel!
   @IBOutlet var gateNr: UILabel!
   @IBOutlet var departingFrom: UILabel!
   @IBOutlet var arrivingTo: UILabel!
   @IBOutlet var planeImage: UIImageView!
-
+  
   @IBOutlet var flightStatus: UILabel!
   @IBOutlet var statusBanner: UIImageView!
-
+  
   let snowView = SnowView(frame: CGRect(x: -150, y: -100, width: 300, height: 50))
-
+  
   // MARK: view controller methods
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     //adjust ui
     summary.addSubview(summaryIcon)
     summaryIcon.center.y = summary.frame.size.height / 2
-
+    
     //add the snow effect layer
     let snowClipView = UIView(frame: view.frame.offsetBy(dx: 0, dy: 50))
     snowClipView.clipsToBounds = true
     snowClipView.addSubview(snowView)
     view.addSubview(snowClipView)
-
+    
     //start rotating the flights
     changeFlight(to: londonToParis)
   }
-
+  
   // MARK: custom methods
-
-  func changeFlight(to data: FlightData) {
+  
+  func changeFlight(to data: FlightData, animated: Bool = false) {
     // populate the UI with the next flight's data
     summary.text = data.summary
     flightNr.text = data.flightNr
@@ -84,12 +84,35 @@ class ViewController: UIViewController {
     departingFrom.text = data.departingFrom
     arrivingTo.text = data.arrivingTo
     flightStatus.text = data.flightStatus
-    bgImageView.image = UIImage(named: data.weatherImageName)
-    snowView.isHidden = !data.showWeatherEffects
-
+    
+    if animated {
+      fade(imageView: bgImageView, toImage: UIImage(named: data.weatherImageName)!, showEffects: data.showWeatherEffects)
+    } else {
+      bgImageView.image = UIImage(named: data.weatherImageName)
+      snowView.isHidden = !data.showWeatherEffects
+    }
+    
     // schedule next flight
     delay(seconds: 3.0) {
-      self.changeFlight(to: data.isTakingOff ? parisToRome : londonToParis)
+      self.changeFlight(to: data.isTakingOff ? parisToRome : londonToParis, animated: true)
     }
+  }
+  
+  func fade(imageView: UIImageView, toImage: UIImage, showEffects: Bool) {
+    UIView.transition(with: imageView, duration: 1.0,
+                      options: .transitionCrossDissolve,
+                      animations: {
+      imageView.image = toImage
+    },
+                      completion: nil
+    )
+    
+    UIView.animate(withDuration: 1.0, delay: 0.0,
+                   options: .curveEaseOut,
+                   animations: {
+      self.snowView.alpha = showEffects ? 1.0 : 0.0
+    },
+                   completion: nil
+    )
   }
 }
